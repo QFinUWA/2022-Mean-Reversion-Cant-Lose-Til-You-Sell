bb3bad7f36a2683574917d00b47cf8b358f3fcc1
23080214 <23080214@student.uwa.edu.au>
2022-04-01 11:39:02 +0800

    Started logic for IBR in main

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def logic(
     if (
         today > training_period
     ):  # If the lookback is long enough to calculate the Bollinger Bands
-
+        '''
         if (
             lookback["close"][today] < lookback["BOLD"][today]
         ):  # If current price is below lower Bollinger Band, enter a long position
@@ -50,6 +50,32 @@ def logic(
                 account.enter_position(
                     "short", account.buying_power, lookback["close"][today]
                 )  # Enter a short position
+        '''
+        IBR = (lookback["close"][today] - lookback["low"][today])/(lookback["high"][today]-lookback["low"][today])
+        
+        if (
+            IBR < 0.2
+        ):
+            for position in account.positions:  # Close all current positions
+                account.close_position(position, 1, lookback["close"][today])
+            if account.buying_power > 0:
+                account.enter_position(
+                    "long", account.buying_power, lookback["close"][today]
+                )  # Enter a long position
+
+        elif (
+            IBR > 0.8
+        ):  # If today's price is above the upper Bollinger Band, enter a short position
+            for position in account.positions:  # Close all current positions
+                account.close_position(position, 1, lookback["close"][today])
+            if account.buying_power > 0:
+                account.enter_position(
+                    "short", account.buying_power, lookback["close"][today]
+                )  # Enter a short position
+
+        else:
+            for position in account.positions:  # Close all current positions
+                account.close_position(position, 1, lookback["close"][today])
 
 
 """
