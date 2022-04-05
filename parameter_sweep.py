c7b4aeaf0d7b3a5d418d006814b185daa7a7038d
Nicholas Choong <nicholascwc1997@gmail.com>
2022-04-06 04:17:14 +0800

    testing rsi logic

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -1,5 +1,11 @@
 # import main
-import mean_reversion_example #Swap with importing main when example is defunct
+# from mean_reversion_example import (
+#     preprocess_data,
+#     logic,
+# )  # Swap with importing main when example is defunct
+
+from logic_functions.rsi import preprocess_data, logic
+
 from backtester import tester
 import pandas as pd
 
@@ -17,18 +23,24 @@ if __name__ == "__main__":
         "TSLA_2020-03-09_2022-01-28_15min",
         "AAPL_2020-03-24_2022-02-12_15min",
     ]  # List of stock data csv's to be tested, located in "data/" folder
-    
+
     # loop over v1 and test for each
-    for training_period in range(2, 52, 2): # Test training periods from 2 to 50 in steps of 2
+    # for training_period in range(
+    #     2, 52, 2
+    # ):  # Test training periods from 2 to 50 in steps of 2
+    for training_period in range(14, 15):
         # for standard_deviations in range(1, 10, 1): # Test standard deviations from 1 to 9 in steps of 1. as an example, Will test each standard deviation for each training period 2-52 in steps of 2.
         standard_deviations = 3.5
-        list_of_stocks_proccessed = mean_reversion_example.preprocess_data(list_of_stocks, v1=training_period, v2=standard_deviations)  # Preprocess the data
+        list_of_stocks_proccessed = preprocess_data(
+            list_of_stocks, v1=training_period, v2=standard_deviations
+        )  # Preprocess the data
         results = tester.test_array(
-            list_of_stocks_proccessed, mean_reversion_example.logic, chart=False, v1=training_period        
-            )
+            list_of_stocks_proccessed, logic, chart=False, v1=training_period
+        )
         print("training period " + str(training_period))
         print("standard deviations " + str(standard_deviations))
-        df = pd.DataFrame(
-            list(results)
-        )  # Create dataframe of results
-        df.to_csv("results/Test_Results.csv", mode='a', header=False, index=False)  # Save results to csv
+        df = pd.DataFrame(list(results))  # Create dataframe of results
+        print(f"DONE!!! {training_period}")
+        df.to_csv(
+            "results/Test_Results.csv", mode="a", header=False, index=False
+        )  # Save results to csv
