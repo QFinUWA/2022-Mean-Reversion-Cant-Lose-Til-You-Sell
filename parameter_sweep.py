e14c328a5c159d31e893fcdca24aa8a9e04df0dc
Nicholas Choong <nicholascwc1997@gmail.com>
2022-04-17 13:34:17 +0800

    added disney stock

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     list_of_stocks = [
         "TSLA_2020-03-09_2022-01-28_15min",
         "AAPL_2020-03-24_2022-02-12_15min",
+        "DIS_2020-04-27_2022-03-18_15min",
         # "TSLA_2020-03-01_2022-01-20_1min",
         # "AAPL_2020-03-24_2022-02-12_1min",
     ]  # List of stock data csv's to be tested, located in "data/" folder
@@ -36,7 +37,7 @@ if __name__ == "__main__":
     # ):  # Test training periods from 2 to 50 in steps of 2
     for training_period in range(14, 15):
         # for standard_deviations in range(1, 10, 1): # Test standard deviations from 1 to 9 in steps of 1. as an example, Will test each standard deviation for each training period 2-52 in steps of 2.
-        standard_deviations = 2.7
+        standard_deviations = 2
         list_of_stocks_proccessed = preprocess_data(
             list_of_stocks, v1=training_period, v2=standard_deviations
         )  # Preprocess the data
